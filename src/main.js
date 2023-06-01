36d3a7903d5983bf2894a1929e0b57b65a13a2ba
christianarca27 <christian.arca.mail@gmail.com>
2023-06-01 15:40:15 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "@fortawesome/fontawesome-free/css/all.css"
+import * as bootstrap from 'bootstrap'
+import './style.scss'
+
+createApp(App).mount('#app')
